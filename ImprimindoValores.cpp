8fc9a8d93ee0cb3c11a8118be7f9789a3b54d738
CarlosEugenio177 <carloseugeniomendes.p.l@gmail.com>
2026-01-14 18:58:40 -0300

    system pause

--- ImprimindoValores.cpp
+++ ImprimindoValores.cpp
@@ -8,4 +8,7 @@ int day = a+b;
 string month = "Janeiro";
 string year = "2026";
     std::cout << "Hello, World! hoje é " << day << " de " << month << " de " << year << std::endl;
+
+    system("pause"); 
+    return 0;
 }
