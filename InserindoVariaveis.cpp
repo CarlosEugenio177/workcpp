7bc12b67302fa6b097d5474d46b6b75f37a76003
CarlosEugenio177 <carloseugeniomendes.p.l@gmail.com>
2026-01-14 19:13:39 -0300

    .

---
+++ InserindoVariaveis.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a; 
+int b;
+cout << "Calculadora Prmitiva";
+cout << "Digite o valor de a: ";
+cin >> a;
+cout << "Digite o valor de b: ";
+cin >> b;
+    std::cout << "O valor de a + b: " << a+b << std::endl;
+    return 0;
+
+}
